fe0fee4f64df6359db6fd3d535259f8d39d68f1f
syeongiicoco <syeongii78@gmail.com>
2021-04-14 09:42:23 +0900

    ADD: scss nesting 완료

--- src/Pages/AccountPage/Login.js
+++ src/Pages/AccountPage/Login.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import '../AccountPage/Login.scss';
 
 class Login extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="loginHome">
+        <div className="icon">
+          <header className="olBox">
+            <ol className="list">
+              <li>
+                <a className="link" href="">
+                  <i class="fas fa-home" />
+                </a>
+              </li>
+              <li>
+                <i class="fas fa-angle-right" />
+              </li>
+              <li className="linkSignUp">로그인</li>
+            </ol>
+          </header>
+        </div>
+        <div className="signUp">
+          <h2>
+            <strong>로그인</strong>
+          </h2>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default Login
+export default Login;
--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SignUp from './Pages/AccountPage/SignUp';
+import Login from './Pages/AccountPage/Login';
 
 export default class Routes extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <SignUp />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
         </Switch>
       </Router>
     );
