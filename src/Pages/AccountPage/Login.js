3102c8766d628d8ff7a88a66b8d49374eaeed9f7
syeongiicoco <syeongii78@gmail.com>
2021-04-14 17:13:33 +0900

    ADD: loginpage layout 완료

--- src/Pages/AccountPage/Login.js
+++ src/Pages/AccountPage/Login.js
@@ -22,31 +22,60 @@ class Login extends Component {
           <div className="loginKo">
             <h2 className="nameLogin">로그인</h2>
           </div>
-          <div className="idPw">
-            <div className="idPwBig">
-              <div className="idBox">
-                <div className="idKo">아이디</div>
-                <div>
-                  <input className="writeId" type="text" placeholder="아이디" />
+          <div className="idPwMom">
+            <div className="idPw">
+              <div className="idPwBig">
+                <div className="idBox">
+                  <div className="idKo">아이디</div>
+                  <div>
+                    <input
+                      className="writeId"
+                      type="text"
+                      placeholder="아이디"
+                    />
+                  </div>
+                </div>
+                <div className="pwBox">
+                  <div className="pwKo">비밀번호</div>
+                  <div>
+                    <input
+                      className="writePw"
+                      type="password"
+                      placeholder="비밀번호"
+                    />
+                  </div>
                 </div>
               </div>
-              <div className="pwBox">
-                <div className="pwKo">비밀번호</div>
-                <div>
-                  <input
-                    className="writePw"
-                    type="password"
-                    placeholder="비밀번호"
-                  />
+              <div>
+                <div className="loginBtBox">
+                  <a className="loginBt" href="">
+                    로그인
+                  </a>
                 </div>
               </div>
             </div>
-            <div>
-              <div className="loginBtBox">
-                <a className="loginBt" href="">
-                  로그인
+            <div className="searchBox">
+              <div className="searchId">
+                <a href="" className="searchIdBox">
+                  아이디 찾기
                 </a>
               </div>
+              <div>|</div>
+              <div className="searchPw">
+                <a href="" className="searchPwBox">
+                  비밀번호 찾기
+                </a>
+              </div>
+            </div>
+            <div className="yet">
+              <p className="general">
+                아직 제너럴브랜즈의 회원이 아니신가요?
+                <br />
+                <span className="service">
+                  회원가입하고 다양한 혜택과 서비스를 이용해보세요!
+                </span>
+              </p>
+              <button className="memberBt">회원가입</button>
             </div>
           </div>
         </div>
--- src/Pages/AccountPage/SignUp.js
+++ src/Pages/AccountPage/SignUp.js
@@ -6,167 +6,164 @@ class SignUp extends Component {
     return (
       <div className="home">
         <div className="icon">
-          <div className="homeIcon">
-            <header className="olBox">
-              <ol className="list">
-                <li>
-                  <a className="link" href="">
-                    <i class="fas fa-home" />
-                  </a>
-                </li>
-                <li>
-                  <i class="fas fa-angle-right" />
-                </li>
-                <li className="linkSignUp">회원 가입</li>
-              </ol>
-            </header>
-            <div className="signUp">
-              <h2>
-                <strong>회원 가입</strong>
-              </h2>
-            </div>
-            <table className="type">
-              <tr>
-                <div className="memberColor">
-                  <td className="division">회원구분*</td>
-                </div>
-                <td className="memberShip">
-                  <input className="own" name="button" type="radio" />
-                  <label for="ownMember">개인회원</label>
-                  <input className="company" name="button" type="radio" />
-                  <label for="companyMember">사업자회원</label>
-                </td>
-              </tr>
-            </table>
-            <div className="inform">
-              <div className="informBox">
-                <h3>기본정보</h3>
+          <header className="olBox">
+            <ol className="list">
+              <li>
+                <a className="link" href="">
+                  <i className="fas fa-home" />
+                </a>
+              </li>
+              <li>
+                <i className="fas fa-angle-right" />
+              </li>
+              <li className="linkSignUp">회원 가입</li>
+            </ol>
+          </header>
+          <div className="signUp">
+            <h2>
+              <strong>회원 가입</strong>
+            </h2>
+          </div>
+          <table className="type">
+            <tr>
+              <div className="memberColor">
+                <td className="division">회원구분*</td>
               </div>
-              <div className="idBox">
-                <div className="oneBox">
-                  <table className="idPw">
-                    <tr>
-                      <td className="idColor">
-                        <div>아이디*</div>
-                      </td>
-                      <td className="idEnglish">
-                        <input className="id" name="information" type="text" />
-                        <div className="english">(영문소문자/숫자,4~16자)</div>
-                      </td>
-                    </tr>
-                  </table>
-                </div>
-                <table>
+              <td className="memberShip">
+                <label for="ownMember">
+                  <input className="own" name="button" type="radio" />
+                  개인회원
+                </label>
+                <label for="companyMember">
+                  <input className="company" name="button" type="radio" />
+                  사업자회원
+                </label>
+              </td>
+            </tr>
+          </table>
+          <div className="inform">
+            <div className="informBox">
+              <h3>기본정보</h3>
+            </div>
+            <div className="idBox">
+              <div className="oneBox">
+                <table className="idPw">
                   <tr>
-                    <td className="pwColor">
-                      <div className="pwKo">비밀번호*</div>
+                    <td className="idColor">
+                      <div>아이디*</div>
                     </td>
-                    <td className="pwBox">
-                      <input
-                        className="pw"
-                        name="information"
-                        type="password"
-                      />
-                      <div className="pwEnglish">
-                        (영문 대소문자/숫자/특수문자 중 2가지 이상
-                        조합,8자~16자)
-                      </div>
+                    <td className="idEnglish">
+                      <input className="id" name="information" type="text" />
+                      <div className="english">(영문소문자/숫자,4~16자)</div>
                     </td>
                   </tr>
                 </table>
-                <div className="write">
-                  <table>
-                    <tr>
-                      <td className="writeName">
-                        <div className="name">이름*</div>
-                      </td>
-                      <td className="naming">
-                        <input
-                          className="nameBox"
-                          name="information"
-                          type="text"
-                        />
-                      </td>
-                    </tr>
-                  </table>
-                </div>
-                <div className="phoneBox">
-                  <table>
-                    <tr>
-                      <td className="writePhone">
-                        <div className="phoneKo">휴대전화*</div>
-                      </td>
-                      <div className="callingBox">
-                        <td className="calling">
-                          <div className="submit">
-                            <input
-                              className="phone"
-                              type="tel"
-                              name="phone"
-                              maxlength="13"
-                            />
-                          </div>
-                        </td>
-                      </div>
-                    </tr>
-                  </table>
-                </div>
-                <div className="emailBox">
-                  <tale>
-                    <tr>
-                      <td className="writeEmail">
-                        <div className="emailKo">이메일*</div>
-                      </td>
-                      <td>
-                        <div className="writing">
-                          <input
-                            className="email"
-                            type="email"
-                            name="userEmail"
-                          />
-                        </div>
-                      </td>
-                    </tr>
-                  </tale>
-                </div>
               </div>
-            </div>
-            <div className="addInform">
-              <h3>추가정보</h3>
-            </div>
-            <table className="plus">
-              <tr>
-                <div className="genderBox">
-                  <div className="gender">
-                    <td>
-                      <div className="other">성별*</div>
-                    </td>
-                  </div>
-                </div>
-                <td className="mw">
-                  <input className="Man" name="buttoni" type="radio" />
-                  <label for="manMember">남자</label>
-                  <input className="Woman" name="buttoni" type="radio" />
-                  <label for="womanMember">여자</label>
-                </td>
-              </tr>
-            </table>
-            <div className="bigAdd">
-              <table className="addTable">
-                <tr className="birthDayBox">
-                  <div>
-                    <div className="birthDay">
-                      <td className="dailyBox">
-                        <div className="birth">생년월일*</div>
-                      </td>
+              <table>
+                <tr>
+                  <td className="pwColor">
+                    <div className="pwKo">비밀번호*</div>
+                  </td>
+                  <td className="pwBox">
+                    <input className="pw" name="information" type="password" />
+                    <div className="pwEnglish">
+                      (영문 대소문자/숫자/특수문자 중 2가지 이상 조합,8자~16자)
                     </div>
-                  </div>
-                  <td className="dailyMonth">
-                    <input className="daily" type="date" />
                   </td>
                 </tr>
               </table>
+              <div className="write">
+                <table>
+                  <tr>
+                    <td className="writeName">
+                      <div className="name">이름*</div>
+                    </td>
+                    <td className="naming">
+                      <input
+                        className="nameBox"
+                        name="information"
+                        type="text"
+                      />
+                    </td>
+                  </tr>
+                </table>
+              </div>
+              <div className="phoneBox">
+                <table>
+                  <tr>
+                    <td className="writePhone">
+                      <div className="phoneKo">휴대전화*</div>
+                    </td>
+                    <div className="callingBox">
+                      <td className="calling">
+                        <div className="submit">
+                          <input
+                            className="phone"
+                            type="tel"
+                            name="phone"
+                            maxlength="13"
+                          />
+                        </div>
+                      </td>
+                    </div>
+                  </tr>
+                </table>
+              </div>
+              <div className="emailBox">
+                <tale>
+                  <tr>
+                    <td className="writeEmail">
+                      <div className="emailKo">이메일*</div>
+                    </td>
+                    <td>
+                      <div className="writing">
+                        <input
+                          className="email"
+                          type="email"
+                          name="userEmail"
+                        />
+                      </div>
+                    </td>
+                  </tr>
+                </tale>
+              </div>
             </div>
+          </div>
+          <div className="addInform">
+            <h3>추가정보</h3>
+          </div>
+          <table className="plus">
+            <tr>
+              <div className="genderBox">
+                <div className="gender">
+                  <td>
+                    <div className="other">성별*</div>
+                  </td>
+                </div>
+              </div>
+              <td className="manWoman">
+                <input className="Man" name="buttoni" type="radio" />
+                <label for="manMember">남자</label>
+                <input className="Woman" name="buttoni" type="radio" />
+                <label for="womanMember">여자</label>
+              </td>
+            </tr>
+          </table>
+          <div className="bigAdd">
+            <table className="addTable">
+              <tr className="birthDayBox">
+                <div>
+                  <div className="birthDay">
+                    <td className="dailyBox">
+                      <div className="birth">생년월일*</div>
+                    </td>
+                  </div>
+                </div>
+                <td className="dailyMonth">
+                  <input className="daily" type="date" />
+                </td>
+              </tr>
+            </table>
           </div>
         </div>
       </div>
