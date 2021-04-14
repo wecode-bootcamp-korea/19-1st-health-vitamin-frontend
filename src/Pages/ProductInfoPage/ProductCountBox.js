d8570aa93967bb11b405338549e52388567dad66
ljh95 <ilegwmam@gmail.com>
2021-04-14 12:21:17 +0900

    잠시 멈춤

--- src/Pages/ProductInfoPage/ProductCountBox.js
+++ src/Pages/ProductInfoPage/ProductCountBox.js
@@ -2,9 +2,48 @@ import React, { Component } from 'react';
 import './ProductCountBox.scss';
 
 export default class ProductCountBox extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      count: 1,
+    };
+  }
+
   upClick = () => {
-    console.log(this.props.item.count);
+    this.setState({
+      count: this.state.count * 1 + 1,
+    });
   };
+
+  downClick = () => {
+    if (this.state.count <= 1) {
+      return;
+    }
+    this.setState({
+      count: this.state.count * 1 - 1,
+    });
+  };
+
+  changeNum = e => {
+    if (/\d/.test(e.key)) {
+      this.setState({
+        count: (this.state.count + e.key) * 1,
+      });
+    } else if (e.keyCode === 8) {
+      let sCount = this.state.count + '';
+      this.setState({
+        count: sCount.slice(0, sCount.length - 1),
+      });
+    }
+  };
+
+  inputOnChange = e => {
+    this.setState({
+      count: e.target.value,
+    });
+  };
+
   render() {
     return (
       <div className="productCountBox">
@@ -12,13 +51,14 @@ export default class ProductCountBox extends Component {
         <div className="inputBox">
           <input
             className="input"
-            type="text"
-            readOnly
-            value={this.props.item.count}
+            type="number"
+            value={this.state.count}
+            onChange={this.inputOnChange}
+            // onKeyDown={this.changeNum}
           />
           <div className="arrowBox">
             <i className="fas fa-sort-up" onClick={this.upClick}></i>
-            <i className="fas fa-sort-down"></i>
+            <i className="fas fa-sort-down" onClick={this.downClick}></i>
           </div>
           <p className="x">x</p>
         </div>
