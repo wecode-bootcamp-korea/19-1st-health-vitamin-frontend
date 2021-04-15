ed86a34b522dcc844374788a431637cc950cc55d
syeongiicoco <syeongii78@gmail.com>
2021-04-15 14:41:38 +0900

    ADD: favorite layout

--- src/Pages/FavoriteProduct/FavoriteProduct.js
+++ src/Pages/FavoriteProduct/FavoriteProduct.js
@@ -1,8 +1,51 @@
 import React, { Component } from 'react';
+import '../FavoriteProduct/FavoriteProduct.scss';
 
 class FavoriteProduct extends Component {
   render() {
-    return <div className="all"></div>;
+    return (
+      <div className="all">
+        <header className="olFavoriteBox">
+          <ol className="favoriteList">
+            <li>
+              <a className="goLink" href="">
+                <i className="fas fa-home" />
+              </a>
+            </li>
+            <li>
+              <i className="fas fa-angle-right" />
+            </li>
+            <li className="goSignUp">마이쇼핑</li>
+            <li>
+              <i className="fas fa-angle-right" />
+            </li>
+            <li className="goSignUp">관심상품</li>
+          </ol>
+        </header>
+        <div className="loginKo">
+          <h2 className="nameLogin">관심상품</h2>
+        </div>
+        <div id="table">
+          <p class="message">관심상품이 내역이 없습니다.</p>
+          {/* <div className="row">
+            <table>
+              <tr>
+                <th scope="col"></th>
+                <th scope="col">이미지</th>
+                <th scope="col">상품정보</th>
+                <th scope="col">판매가</th>
+                <th scope="col">적립금</th>
+                <th scope="col">배송구분</th>
+                <th scope="col">배송비</th>
+                <th scope="col">합계</th>
+                <th scope="col">선택</th>
+              </tr>
+              <tr></tr>
+            </table>
+          </div> */}
+        </div>
+      </div>
+    );
   }
 }
 
--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SignUp from './Pages/AccountPage/SignUp';
 import Login from './Pages/AccountPage/Login';
+import FavoriteProduct from './Pages/FavoriteProduct/FavoriteProduct';
 
 export default class Routes extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class Routes extends Component {
         <Switch>
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
+          <Route exact path="/favoriteproduct" component={FavoriteProduct} />
         </Switch>
       </Router>
     );
